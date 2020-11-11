9f1d3ed52f8e9fd5cbb9145f9a0849089ee4fcbd
Thibaud <carruptt@gmail.com>
2020-11-11 22:50:46 +0100

    add level02

---
+++ Assets/Scripts/DontDestroyOnLoadScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoadScreen : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Awake()
+    {
+       foreach (var element in objects)
+        {
+            DontDestroyOnLoad(element);
+        } 
+    }
+}
