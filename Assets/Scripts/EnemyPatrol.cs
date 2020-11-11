7d4e74e3079e34812b0a38353187430d11a7a8e2
Thibaud_Carrupt <thibaud.carrupt@students.hevs.ch>
2020-11-11 15:27:01 +0100

    add healthbar damages kill monsters

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed;
+    public Transform[] waypoints;
+    public SpriteRenderer spriteRenderer;
+    public int damageOnCollision;
+
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        target = waypoints[0];
+        spriteRenderer.flipX = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damageOnCollision);
+        }
+    }
+}
