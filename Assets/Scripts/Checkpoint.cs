07dae2ef1fb0b7619dd79fd0dea5dbd54ecc4c44
Thibaud <carruptt@gmail.com>
2020-11-12 16:26:29 +0100

    add Checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Transform playerSpawn;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerSpawn.position = transform.position;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 0f);
+        }
+    }
+}
--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -1,14 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
     public string sceneName;
+    [HideInInspector] public Animator fade;
+
+    private void Awake()
+    {
+        fade = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine("loadNextLevel");
         }
+    }
+
+    public IEnumerator loadNextLevel()
+    {
+        fade.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
     }
 }
